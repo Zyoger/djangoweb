376e62d32a513b14f5834a9ba5751dc07513fb22
zyoge <zyoger@gmail.com>
2022-09-15 20:55:11 +0300

    Починил главную страницу

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('', views.index, name="index"),
     path('aboutme', views.about, name="about"),
     path('progects', views.works, name="works"),
     path('contacts', views.contacts, name="contacts")
